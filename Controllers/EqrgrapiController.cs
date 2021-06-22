37a4f13857cbddbd8596f095bf18fa8d9bdd395d
Andrea Archie <aarchie@eqr.com>
2021-06-22 13:38:05 -0500

    added EqrgrapiController.cs to EQR.DDS.Azure.Gateway.Lease

---
+++ Controllers/EqrgrapiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EQR.DDS.Azure.Gateway.Lease.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EqrgrapiController : ControllerBase
+    {
+        private readonly ILogger<EqrgrapiController> _logger;
+        public EqrgrapiController(ILogger<EqrgrapiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("ping")]
+        public string Ping()
+        {
+
+            return "Lease Gateway API is running.";
+        }
+    }
+}
